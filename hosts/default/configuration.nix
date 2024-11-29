bfb8d4ff42e57e2c596d098399537a4e179510be
hangocdinhnew <dinhndrwnew@gmail.com>
2024-11-29 11:55:07 +0700

    Add some stuff

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -175,10 +175,14 @@
     qemu_full
     devenv
     tmux
+    vulkan-tools
   ];
 
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+
+  # Fix some programs
+  programs.nix-ld.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
