0f8ade0c4bcd4ce5ca9b22b554ccc1589064815e
hangocdinhnew <dinhndrwnew@gmail.com>
2025-10-23 19:43:44 +0700

    lmao.

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -247,6 +247,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
 
